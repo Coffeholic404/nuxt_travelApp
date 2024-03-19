df07be1365598af2de7c383c7f6fe1a696b52a72
Mohammed Taqi <taqealibade@gmail.com>
2024-03-19 03:29:34 +0300

    Adding assets and meta tags

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+<script setup>
+  useHead({
+    title: "Travel",
+    meta: [
+    { name: 'description', content: 'Travel UI/UX App For Camping' }
+  ],
+  })
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class=" text-teal-500">Hello Nuxt</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
