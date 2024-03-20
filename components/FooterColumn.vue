c2d4c1fbd35c0016767619a14afc38049074b3de
Mohammed Taqi <taqealibade@gmail.com>
2024-03-20 06:30:04 +0300

    finishing TheFooter component

---
+++ components/FooterColumn.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col gap-5">
+        <h4 class="bold-18 whitespace-nowrap">
+            {{ props.title }}
+        </h4>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    require: true,
+    type: String
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,83 @@
 <template>
-    <div>
+    <footer class="flexCenter mb-24">
+        <div class="padding-container max-container flex w-full flex-col gap-14">
+            <div class="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+                <NuxtLink to="/" class="mb-10">
+                    <NuxtImg 
+                    src="hilink-logo.svg"
+                    alt="logo"
+                    width="74"
+                    height="29"
+                    />
+                </NuxtLink>
 
-    </div>
+                <div class="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+                    <FooterColumn
+                    v-for="column in footerLinks"
+                    :title="column.title"
+                    >
+                    <ul class=" regular-14 flex flex-col gap-4 text-gray-30">
+                        <NuxtLink to="/" v-for="link in column.links" :key="link">
+                            {{ link }}
+                        </NuxtLink>
+                    </ul>
+                    </FooterColumn>
+
+                    <div class="flex flex-col gap-5">
+                        <FooterColumn
+                        :title="contactInfo.title"
+                        >
+                        <NuxtLink
+                        to="/"
+                        v-for="link in contactInfo.links" key="link"
+                        class=" flex gap-4 md:flex-col lg:flex-row"
+                        >
+                        <p class=" whitespace-nowrap">
+                            {{ link.label }}:
+                        </p>
+                        <p class="medium-14 whitespace-nowrap text-blue-70">{{ link.value }}</p>
+                        </NuxtLink>
+                        </FooterColumn>
+                    </div>
+
+                    <div class="flex flex-col gap-5">
+                        <FooterColumn
+                        :title="socials.title"
+                        >
+                        <ul class="regular-14 flex gap-4 text-gray-30">
+                            <NuxtLink
+                            v-for="link in socials.links"
+                            :key="link"
+                            to="/"
+                            >
+                            <NuxtImg 
+                            :src="link"
+                            alt="logo"
+                            width="24"
+                            height="24"
+                            />
+                            </NuxtLink>
+
+                        </ul>
+                        </FooterColumn>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="border bg-gray-20"/>
+            <p class="regular-14 w-full text-center text-gray-30">2024 Hilink | All right reserved</p>
+        </div>
+    </footer>
 </template>
 
 <script setup lang="ts">
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '~/constants';
 
+const footerLinks = FOOTER_LINKS.map((column) => column)
+const contactInfo = FOOTER_CONTACT_INFO
+const socials = SOCIALS
 </script>
 
 <style scoped>
